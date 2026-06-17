f5ed6769de81ebe3bbeb7804a9e4eaedaf6de677
hampohampoham-pehe <yrprle.aditmk.400@outlook.com>
2026-06-17 13:17:15 +0530

    Integrate MTProto Proxy and update server response

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const http = require('http');
-const { exec } = require('child_process');
+const MTProtoProxy = require('telegram-mtproto-proxy');
 
-// 1. This keeps Render happy by running a website
+// 1. Web server to keep Render happy
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Proxy is running!\n');
+  res.end('Proxy Alive\n');
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Web server listening on port ${PORT}`);
-});
+server.listen(PORT);
 
-// 2. This starts the actual Telegram Proxy (Using a Node.js MTProto library)
-// Note: You will need to add 'telegram-mtproto-proxy' to a package.json file.
+// 2. Launch MTProto Proxy on port 443
+const proxy = new MTProtoProxy({
+  port: 443,
+  secret: 'b0a551ac8b1b11e29e910242ac110002' // Simple 32-character hex secret
+});
+proxy.start();
